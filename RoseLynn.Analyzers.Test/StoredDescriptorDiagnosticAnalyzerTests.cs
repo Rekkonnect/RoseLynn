beda9b1e27d1a6d64242370ad55d49273b3a1d69
Alex <kalfas.alex@gmail.com>
2021-09-04 02:29:23 +0300

    Add supported diagnostic tests for stored descriptor analyzer

---
+++ RoseLynn.Analyzers.Test/StoredDescriptorDiagnosticAnalyzerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using RoseLynn.Analyzers.Test.MockedResources;
+
+namespace RoseLynn.Analyzers.Test
+{
+    public class StoredDescriptorDiagnosticAnalyzerTests
+    {
+        [Test]
+        public void SupportedDiagnosticsTest()
+        {
+            var analyzerInstance = new MockAnalyzer();
+            Assert.AreEqual(MockStorage.Instance.GetDiagnosticDescriptors(typeof(MockAnalyzer)).Length, analyzerInstance.SupportedDiagnostics.Length);
+        }
+    }
+}
